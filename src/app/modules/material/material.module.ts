2f67261b654697169961e38272cf892596e2cc82
Wojciech Szućko <wojciech.szucko@gmail.com>
2018-10-06 18:58:26 +0200

    feat: dodanie moment date adapter

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import {
+	DateAdapter,
+	MAT_DATE_FORMATS,
+	MAT_DATE_LOCALE,
 	MatButtonModule,
 	MatCardModule,
+	MatDatepickerModule,
 	MatDialogModule,
 	MatFormFieldModule,
 	MatInputModule,
 	MatProgressBarModule,
 	MatSelectModule
 } from '@angular/material';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+export const MY_FORMATS = {
+	parse: {
+		dateInput: 'YYYY-MM-DD',
+	},
+	display: {
+		dateInput: 'YYYY-MM-DD',
+		monthYearLabel: 'MMM YYYY',
+		dateA11yLabel: 'YYYY-MM-DD',
+		monthYearA11yLabel: 'MMMM YYYY',
+	},
+};
 
 @NgModule({
 	imports: [
@@ -18,6 +35,7 @@ import {
 		MatFormFieldModule,
 		MatProgressBarModule,
 		MatCardModule,
+		MatDatepickerModule,
 	],
 	exports: [
 		MatDialogModule,
@@ -27,6 +45,12 @@ import {
 		MatFormFieldModule,
 		MatProgressBarModule,
 		MatCardModule,
+		MatDatepickerModule,
+	],
+	providers: [
+		{ provide: MAT_DATE_LOCALE, useValue: 'pl-PL' },
+		{ provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+		{ provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
 	]
 })
 export class MaterialModule {
