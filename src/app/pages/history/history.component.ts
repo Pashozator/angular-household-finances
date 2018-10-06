d72b72aeea4fa49178c5de6f84b9fc0da40eb405
Wojciech Szućko <wojciech.szucko@gmail.com>
2018-10-06 19:39:31 +0200

    feat: sortowanie operacji po dacie

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -4,6 +4,7 @@ import { Operation } from '../../types/operation';
 import { select, Store } from '@ngrx/store';
 import { AppState } from '../../store/state/app.state';
 import { selectOperations } from '../../store/selectors/app.selectors';
+import { map } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-history',
@@ -14,7 +15,10 @@ export class HistoryComponent implements OnInit {
 	public operations$: Observable<Operation[]>;
 
 	constructor(private store: Store<AppState>) {
-		this.operations$ = store.pipe(select(selectOperations));
+		this.operations$ = store.pipe(
+			select(selectOperations),
+			map(operations => operations.sort((a, b) => a.date < b.date ? 1 : -1))
+		);
 	}
 
 	ngOnInit() {
