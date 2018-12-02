c9a6cad4a64f247236a23e9dc63ae4144502b2c5
Wojciech Szućko <wojciech.szucko@mentax.pl>
2018-12-02 17:33:53 +0100

    feat: sort goals by date

--- src/app/store/adapters/goals.adapter.ts
+++ src/app/store/adapters/goals.adapter.ts
@@ -1,4 +1,10 @@
 import { createEntityAdapter, EntityAdapter } from '@ngrx/entity';
 import { Goal } from '../../types/goal';
 
-export const goalAdapter: EntityAdapter<Goal> = createEntityAdapter<Goal>();
+const selectGoalId = (goal: Goal): string => goal.id;
+const sortByDate = (prev: Goal, next: Goal): number => prev.date < next.date ? 1 : -1;
+
+export const goalAdapter: EntityAdapter<Goal> = createEntityAdapter<Goal>({
+	selectId: selectGoalId,
+	sortComparer: sortByDate
+});
--- src/app/types/goal.ts
+++ src/app/types/goal.ts
@@ -4,7 +4,9 @@ export class Goal {
 		public label: string = '',
 		public description: string = '',
 		public value: number = null,
-		public realized: boolean = false
+		public realized: boolean = false,
+		public date: Date = null
 	) {
+		this.date = date === null ? new Date() : date;
 	}
 }
