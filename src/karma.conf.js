1e9341766c5f314f67da12eed9a83cb4564cf2b8
Wojciech Szućko <wojciech.szucko@gmail.com>
2018-12-10 21:55:28 +0100

    tests: use chrome headless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -23,12 +23,12 @@ module.exports = function (config) {
 			reports: ['html', 'lcovonly'],
 			fixWebpackSourcePaths: true
 		},
-		reporters: ['progress', 'kjhtml'],
+		reporters: ['progress'],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome'],
-		singleRun: false
+		browsers: ['ChromeHeadless'],
+		singleRun: true
 	});
 };
